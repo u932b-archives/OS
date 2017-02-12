058aaf8cf07fae6ba0707da9aa7b715f934905c6
u932b <curiojustus@gmx.com>
2017-02-11 21:56:03 -0500

    Now it behave for the first two input which has random number of delimiters.

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -25,83 +25,116 @@ int main(int argc, char *argv[])
     char output_file[] = "out.list";
     char c;
     typedef enum {def_list, use_list, prog_txt} section;
-    int new_section = 1;
 
     ifp = fopen(input_file, mode);
     if (ifp)
     {
+        int new_section = 1;
+        int fookin_delim = 0;
         section pass1_section = def_list;
         int section_counter = 0;
-        int defpair_count = -1;
+        int symbol_count = -1;
         char* tmp_arr;
         int combo = 0;
         tmp_arr = malloc((combo+1)*sizeof(c));
         while ((c = getc(ifp)) != EOF)
         {
-            if (new_section == 1)
+            if (new_section == 1 && c != '\t' && c != '\n' && c != ' ')
             {
-                // If the section_counter is 0, it means we are going into a new
-                // module
-                if (c == 0)
+                printf ("We are at section: %d\n", (int)pass1_section);
+                // printf("c is now: %c\n", c);
+                if (c == '0')
                 {
-                    pass1_section = def_list;
-                    new_section = 0;
+                    printf ("Hitting zero\n");
+                    pass1_section++;
+                    pass1_section = pass1_section % 3;
+                    section_counter = 0;
+                    // printf ("We are at section: %d\n", (int)pass1_section);
                 }
                 else
                 {
                     section_counter = (int)c - '0';
                     new_section = 0;
                 }
+                // expected delimiter after count declaring
+                fookin_delim = 1;
                 continue;
             }
             if (c != '\t' && c != '\n' && c != ' ')
             {
+                // printf ("section_counter is: %d\n", section_counter);
+
+                fookin_delim = 0;
 
                 switch (pass1_section)
                 {
                     case def_list:
-                        printf ("section_counter is: %d\n", section_counter);
-                        if (section_counter > 0 && defpair_count == -1 )
+                        if (section_counter > 0 && symbol_count == -1 )
                         {
                             // I have to deal with this many symbols before next
                             // section
-                            defpair_count = section_counter*2;
+                            symbol_count = section_counter*2;
                         }
-                        if (defpair_count > 0)
-                        {
-                            printf ("defpair_count is: %d\n", defpair_count);
-                            combo++;
-                            // printf ("c is now: %c\n", c);
-                            tmp_arr = realloc(tmp_arr, (combo+1)*sizeof(c));
-                            tmp_arr[combo-1] = c;
-                            tmp_arr[combo] = '\0';
-                            // printf ("building up the tmp_arr: %s, combo is "
-                            //         "currently: %d\n", tmp_arr, combo);
-                            // putchar(c);
-                            defpair_count--;
-                        int i;
-                        }
-                        else
-                        {
-                            printf ("moving on to the next section.\n");
-                            pass1_section = use_list;
-                        }
+                        combo++;
+                        tmp_arr = realloc(tmp_arr, (combo+1)*sizeof(c));
+                        tmp_arr[combo-1] = c;
+                        tmp_arr[combo] = '\0';
+                        // printf ("building up the tmp_arr: %s, combo is "
+                        //         "currently: %d\n", tmp_arr, combo);
+                        break;
                     case use_list:
-                        pass;
+                        if (section_counter > 0 && symbol_count == -1 )
+                        {
+                            symbol_count = section_counter;
+                        }
+                        combo++;
+                        tmp_arr = realloc(tmp_arr, (combo+1)*sizeof(c));
+                        tmp_arr[combo-1] = c;
+                        tmp_arr[combo] = '\0';
+                        // printf ("building up the tmp_arr: %s, combo is "
+                        //         "currently: %d\n", tmp_arr, combo);
+                        break;
                     case prog_txt:
-                        pass;
+                        if (section_counter > 0 && symbol_count == -1 )
+                        {
+                            symbol_count = section_counter * 2;
+                        }
+                        combo++;
+                        tmp_arr = realloc(tmp_arr, (combo+1)*sizeof(c));
+                        tmp_arr[combo-1] = c;
+                        tmp_arr[combo] = '\0';
+                        // printf ("building up the tmp_arr: %s, combo is "
+                        //         "currently: %d\n", tmp_arr, combo);
+                        break;
                     default:
                         pass;
                 }
             }
             else
+            // We hit an delimiter, time to flush tmp_arr
             {
-                combo = 0;
-                // printf ("WTF\n");
-                printf ("Delimiter. tmp_arr is: %s\n", tmp_arr);
-                free(tmp_arr);
-                tmp_arr = malloc((combo+1)*sizeof(c));
-                tmp_arr[1] = '\0';
+                if (fookin_delim == 0)
+                {
+                    combo = 0;
+                    printf ("%s\n", tmp_arr);
+                    free(tmp_arr);
+                    tmp_arr = malloc((combo+1)*sizeof(c));
+                    tmp_arr[1] = '\0';
+                    if (symbol_count!= -1)
+                    {
+                        symbol_count--;
+                        if (symbol_count == 0)
+                        {
+                            // printf ("moving on to the next section.\n");
+                            symbol_count = -1;
+                            pass1_section++;
+                            pass1_section = pass1_section % 3;
+                            *(&new_section) = 1;
+                        }
+                    }
+                }
+                fookin_delim = 1;
+                // continue;
             }
         }
         fclose(ifp);
@@ -110,18 +143,7 @@ int main(int argc, char *argv[])
     {
         fprintf (stderr, "Can't open file %s\n", input_file);
         exit (1);
-    }/*
-    else
-    {
-        // char buff[255];
-        int a, b;
-        char* string1;
-
-        fscanf(ifp, "%d %s %d", &a, &string1, &b);
-        printf ("%d %d\n", a, b);
-        printf ("%s\n", string1);
-    }*/
-    // fclose(ifp);
+    }
 
     ofp = fopen (output_file, "w");
     if (ofp == NULL)
