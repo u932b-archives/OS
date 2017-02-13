588cc03671278a2e4b25db68826fc849badea412
u932b <curiojustus@gmx.com>
2017-02-12 22:59:47 -0500

    Somehow this beautiful dynamic allocated strucy array does not work.

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -40,9 +40,22 @@ int main(int argc, char *argv[])
     typedef enum {def_list, use_list, prog_txt} section;
     int max_arr_size = 17;
 
+    typedef struct symbol
+    {
+       char* key;
+       int value;
+    } a_symbol;
+    int t_size = 0;
+
+    a_symbol* symbol_table = (a_symbol*) malloc(sizeof(a_symbol) * t_size);
+
+    // a_symbol.key = 1;
+    // a_symbol.value = "foo";
+
     ifp = fopen(input_file, mode);
     if (ifp)
     {
+        // flags
         int new_section = 1;
         int fookin_delim = 0;
         int expect_symbol = 0;
@@ -50,13 +63,17 @@ int main(int argc, char *argv[])
 
         section pass1_section = def_list;
 
+        // counters
         int section_counter = 0;
         int symbol_count = -1;
+        int base_address = 0;
 
+        // tmp array
         char* tmp_arr;
         int combo = 0;
         tmp_arr = malloc((combo+1)*sizeof(c));
         tmp_arr[combo] = '\0';
+
 
         while ((c = getc(ifp)) != EOF)
         {
@@ -69,7 +86,7 @@ int main(int argc, char *argv[])
                 {
                     // This would print as many times as the number of char
                     // in the section_counter
-                    printf ("We are at section: %d\n", (int)pass1_section);
+                    // printf ("We are at section: %d\n", (int)pass1_section);
                     combo++;
                     tmp_arr = realloc(tmp_arr, (combo+1)*sizeof(c));
                     tmp_arr[combo-1] = c;
@@ -89,7 +106,7 @@ int main(int argc, char *argv[])
                                 // This many symbols before next section
                                 symbol_count = section_counter*2;
                             }
-                            printf ("symbol_count is: %d\n", symbol_count);
+                            // printf ("symbol_count is: %d\n", symbol_count);
                             if (symbol_count % 2 == 1 && !isdigit(c))
                             {
                                 printf ("Error: Should be a digit instead:"
@@ -118,11 +135,13 @@ int main(int argc, char *argv[])
                             tmp_arr[combo] = '\0';
                             // printf ("building up the tmp_arr: %s, combo is "
                             //         "currently: %d\n", tmp_arr, combo);
+                            printf ("use_list to symbol_table3: %s\n", symbol_table[0].key);
                             break;
                         case prog_txt:
                             if (section_counter > 0 && symbol_count == -1 )
                             {
                                 symbol_count = section_counter * 2;
+                                base_address += section_counter;
                             }
                             // printf ("symbol_count is: %d\n", symbol_count);
                             combo++;
@@ -159,7 +178,7 @@ int main(int argc, char *argv[])
                     sscanf(tmp_arr, "%d", &section_counter);
                     if (section_counter == 0)
                     {
-                        printf ("Hitting zero\n");
+                        // printf ("Hitting zero\n");
                         pass1_section++;
                         pass1_section = pass1_section % 3;
                         section_counter = 0;
@@ -180,12 +199,44 @@ int main(int argc, char *argv[])
                 }
                 if (fookin_delim == 0)
                 {
+                    // Gotta write to symbol_table
+                    if (pass1_section == 0)
+                    {
+                        if (symbol_count % 2 == 0)
+                        {
+                            t_size++;
+                            symbol_table = (a_symbol*) realloc(symbol_table,
+                                t_size*sizeof(a_symbol));
+                            // symbol_table[t_size-1].key = malloc(sizeof(tmp_arr));
+                            printf ("t_size: %d\n", t_size);
+                            // symbol_table[t_size-1].key = tmp_arr;
+                            symbol_table[0].key = tmp_arr;
+                            // printf ("%s\n", tmp_arr);
+                            printf ("write key to symbol_table2: %s\n", symbol_table[t_size-1].key);
+                            printf ("write key to symbol_table3: %s\n", symbol_table[0].key);
+                        }
+                        else if (symbol_count % 2 == 1)
+                        {
+                            // somehow sscanf not working
+                            // int i;
+                            // printf ("sscanf: %d\n", sscanf(tmp_arr, "%d",&i));
+                            printf (" yo keypair in symbol_table1: %s\n",
+                            symbol_table[0].key);
+                            symbol_table[t_size-1].value = base_address +
+                                atoi(tmp_arr);
+                            printf ("write val to symbol_table: %s\n", tmp_arr);
+                            printf ("keypair in symbol_table1: %d\n",
+                            symbol_table[0].value);
+                        }
+                    }
+
                     // flush
                     combo = 0;
-                    printf ("%s\n", tmp_arr);
+                    // printf ("%s\n", tmp_arr);
                     free(tmp_arr);
                     tmp_arr = malloc((combo+1)*sizeof(c));
                     tmp_arr[1] = '\0';
+
                     if (symbol_count!= -1)
                     {
                         symbol_count--;
@@ -226,6 +277,12 @@ int main(int argc, char *argv[])
         exit (1);
     }
 
+    int i;
+    for (i = 0; i < t_size; i++)
+    {
+        printf ("keypair in symbol_table: %s, %d\n",
+        symbol_table[i].key, symbol_table[i].value);
+    }
     ofp = fopen (output_file, "w");
     if (ofp == NULL)
     {
