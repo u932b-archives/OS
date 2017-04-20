a3dbe7bf200b4c4330b6362048c2cb74847da781
u932b <curiojustus@gmx.com>
2017-04-19 23:46:14 -0400

    version update before change to pte_ptr, might break something

--- hw3/lab3_assign/src/mmu.cpp
+++ hw3/lab3_assign/src/mmu.cpp
@@ -10,6 +10,7 @@ using namespace std;
 vector <int> randvals;
 int inst_count = 0;
 
+int hand=0;
 
 struct pte
 {
@@ -18,6 +19,7 @@ struct pte
     int REFERENCED:1;
     int PAGEDOUT:1;
     int idx:8;
+    int frame_index:8;
 };
 
 struct stats
@@ -49,31 +51,41 @@ vector <frame> frametable;
 vector <frame *> freelist;
 vector <int> fifo_queue;
 
-
-void UNMAP(frame* frame_to_replace, stats* stat)
+void UNMAP(frame* frame_to_replace, stats* stat, bool output)
 {
-    cout << inst_count << ": UNMAP" << setw(4) << setfill(' ') << frame_to_replace->pte_ptr->idx << setw(4) << setfill(' ') << frame_to_replace->page_index << endl;
+    if (output)
+    {
+        cout << inst_count << ": UNMAP" << setw(4) << setfill(' ') << frame_to_replace->pte_ptr->idx << setw(4) << setfill(' ') << frame_to_replace->page_index << endl;
+    }
     stat->unmaps++;
 }
 
-void OUT(frame* frame_to_replace, stats* stat)
+void OUT(frame* frame_to_replace, stats* stat, bool output)
 {
+    if (output)
+    {
     cout << inst_count << ": OUT" << setw(6) << setfill(' ') << frame_to_replace->pte_ptr->idx << setw(4) << setfill(' ') << frame_to_replace->page_index << endl;
+    }
     frame_to_replace->pte_ptr->PAGEDOUT = 1;
     frame_to_replace->pte_ptr->MODIFIED = 0;
     stat->outs++;
 }
 
-void IN(pte *curr_pte, frame* curr_frame, stats* stat)
+void IN(pte *curr_pte, frame* curr_frame, stats* stat, bool output)
 {
-    cout << inst_count << ": IN" << setw(7) << setfill(' ') << curr_pte->idx << setw(4) << setfill(' ') << curr_frame->page_index << endl;
-    // curr_pte->MODIFIED = 0;
+    if (output)
+    {
+        cout << inst_count << ": IN" << setw(7) << setfill(' ') << curr_pte->idx << setw(4) << setfill(' ') << curr_frame->page_index << endl;
+    }
     stat->ins++;
 }
 
-void MAP(pte *curr_pte, frame* curr_frame, stats* stat)
+void MAP(pte *curr_pte, frame* curr_frame, stats* stat, bool output)
 {
-    cout << inst_count << ": MAP" << setw(6) << setfill(' ') << curr_pte->idx << setw(4) << setfill(' ') << curr_frame->page_index << endl;
+    if (output)
+    {
+        cout << inst_count << ": MAP" << setw(6) << setfill(' ') << curr_pte->idx << setw(4) << setfill(' ') << curr_frame->page_index << endl;
+    }
     stat->maps++;
     curr_pte->PRESENT = 1;
     curr_pte->REFERENCED = 1;
@@ -81,12 +93,14 @@ void MAP(pte *curr_pte, frame* curr_frame, stats* stat)
     fifo_queue.push_back(curr_frame->page_index);
 }
 
-void ZERO(frame* curr_frame, stats* stat)
+void ZERO(frame* curr_frame, stats* stat, bool output)
 {
     //zero’d (i.e. NOT PRESENT and NOT PAGEDOUT
     pte *curr_pte = curr_frame->pte_ptr;
-    // cout << "ZERO"<< setw(9) << setfill(' ') << curr_pte.idx << endl;
-    cout << inst_count << ": ZERO"<< setw(9) << setfill(' ') << curr_frame->page_index << endl;
+    if (output)
+    {
+        cout << inst_count << ": ZERO"<< setw(9) << setfill(' ') << curr_frame->page_index << endl;
+    }
     stat->zeros++;
     if (curr_pte != NULL || curr_pte != nullptr)
     {
@@ -111,19 +125,216 @@ class Pager {
 
 class FIFO : public Pager{
     public:
-        frame* allocate_frame(frame* frame_old, stats* stat)
+        frame* allocate_frame(frame* frame_old, stats* stat, bool output)
         {
             frame* frame_to_replace = &frametable[fifo_queue.front()];
             frame_to_replace->pte_ptr->PRESENT = 0;
             fifo_queue.erase (fifo_queue.begin());
-            UNMAP(frame_to_replace, stat);
+            UNMAP(frame_to_replace, stat, output);
             if (frame_to_replace->pte_ptr->MODIFIED != 0)
             {
-                OUT(frame_to_replace, stat);
+                OUT(frame_to_replace, stat, output);
             }
             return frame_to_replace;
         }
 };
+
+int ofs = 0;
+int myrandom(int framesize)
+{
+    ofs = ofs % randvals.size();
+    int to_return = (randvals[ofs] % framesize);
+    // cout << to_return << endl;
+    ofs++;
+    return to_return;
+}
+
+class RANDOM : public Pager{
+    public:
+        frame* allocate_frame(frame* frame_old, stats* stat, int framesize, bool output)
+        {
+            frame* frame_to_replace = new frame(-1);
+            while (frame_to_replace->pte_ptr == nullptr||frame_to_replace->pte_ptr == NULL )
+            {
+                int index = myrandom(framesize);
+                // cout << index << endl;
+                frame_to_replace = &frametable[index];
+                // cout << frame_to_replace->pte_ptr->idx << endl;
+            }
+            frame_to_replace->pte_ptr->PRESENT = 0;
+            // fifo_queue.erase (fifo_queue.begin());
+            UNMAP(frame_to_replace, stat, output);
+            if (frame_to_replace->pte_ptr->MODIFIED != 0)
+            {
+                OUT(frame_to_replace, stat, output);
+            }
+            return frame_to_replace;
+        }
+};
+
+class NRU : public Pager{
+    public:
+        vector <int> class_0;
+        vector <int> class_1;
+        vector <int> class_2;
+        vector <int> class_3;
+        vector <vector<int>*> nru_table = {&class_0, &class_1, &class_2, &class_3};
+        frame* allocate_frame(frame* frame_old, stats* stat, int framesize, bool output)
+        {
+            frame* frame_to_replace = new frame(-1);
+            // go through the whole frametable
+            vector <int>* curr_class;
+            for (int i=0; i<frametable.size(); i++)
+            {
+                pte *curr_pte = frametable[i].pte_ptr;
+                if (curr_pte->REFERENCED == 0 && curr_pte->MODIFIED == 0)
+                {
+                    class_0.push_back(i);
+                }
+                else if (curr_pte->REFERENCED == 0 && curr_pte->MODIFIED != 0)
+                {
+                    class_1.push_back(i);
+                }
+                else if (curr_pte->REFERENCED != 0 && curr_pte->MODIFIED == 0)
+                {
+                    class_2.push_back(i);
+                }
+                else if (curr_pte->REFERENCED != 0 && curr_pte->MODIFIED != 0)
+                {
+                    class_3.push_back(i);
+                }
+            }
+            int class_counter = 0;
+            while (class_counter<4 && frame_to_replace->pte_ptr == nullptr)
+            {
+                curr_class = nru_table[class_counter];
+                //cout << curr_class->size() << endl;
+                if (curr_class->size() > 0 )
+                {
+                    int index = myrandom(curr_class->size());
+                    int frame_index = (*curr_class)[index];
+                    frame_to_replace = &frametable[frame_index];
+                    break;
+                }
+                else
+                {
+                    class_counter++;
+                }
+            }
+            frame_to_replace->pte_ptr->PRESENT = 0;
+            UNMAP(frame_to_replace, stat, output);
+            if (frame_to_replace->pte_ptr->MODIFIED != 0)
+            {
+                OUT(frame_to_replace, stat, output);
+            }
+            return frame_to_replace;
+        }
+};
+
+class SC : public Pager{
+    public:
+        frame* allocate_frame(frame* frame_old, stats* stat, bool output)
+        {
+            // int curr_frame_idx = fifo_queue.front();
+            frame* frame_to_replace = &frametable[fifo_queue.front()];
+            // fifo_queue.push_back(curr_frame_idx);
+            // fifo_queue.erase (fifo_queue.begin());
+            while (frame_to_replace->pte_ptr->REFERENCED!=0)
+            {
+                int curr_frame_idx = fifo_queue.front();
+                frame_to_replace->pte_ptr->REFERENCED=0;
+                fifo_queue.erase (fifo_queue.begin());
+                fifo_queue.push_back(curr_frame_idx);
+                // fifo_queue.push_back(frame_to_replace->pte_ptr->idx);
+                frame_to_replace = &frametable[fifo_queue.front()];
+                // fifo_queue.erase (fifo_queue.begin());
+            }
+            // Risky fix
+            fifo_queue.erase (fifo_queue.begin());
+
+            frame_to_replace->pte_ptr->PRESENT = 0;
+            UNMAP(frame_to_replace, stat, output);
+            if (frame_to_replace->pte_ptr->MODIFIED != 0)
+            {
+                OUT(frame_to_replace, stat, output);
+            }
+            return frame_to_replace;
+        }
+};
+class CLOCK_PHYS: public Pager{
+    public:
+        frame* allocate_frame(frame* frame_old, stats* stat, bool output)
+        {
+            frame* frame_to_replace = &frametable[fifo_queue.front()];
+            while (frame_to_replace->pte_ptr->REFERENCED!=0)
+            {
+                int curr_frame_idx = fifo_queue.front();
+                frame_to_replace->pte_ptr->REFERENCED=0;
+                fifo_queue.erase (fifo_queue.begin());
+                fifo_queue.push_back(curr_frame_idx);
+                frame_to_replace = &frametable[fifo_queue.front()];
+            }
+            // hand is wrong
+            hand = (hand + 1) % frametable.size();
+            // Risky fix
+            fifo_queue.erase (fifo_queue.begin());
+
+            frame_to_replace->pte_ptr->PRESENT = 0;
+            UNMAP(frame_to_replace, stat, output);
+            if (frame_to_replace->pte_ptr->MODIFIED != 0)
+            {
+                OUT(frame_to_replace, stat, output);
+            }
+            return frame_to_replace;
+        }
+};
+
+class CLOCK_VIRTUAL: public Pager{
+    public:
+        frame* allocate_frame(frame* frame_old, stats* stat, bool output)
+        {
+            frame* frame_to_replace;
+            while (true)
+            {
+                // cout << "hand: " << hand << endl;
+                pte* curr_pte = &pagetable[hand];
+                if (curr_pte->PRESENT == 0)
+                {
+                    // hand++;
+                    hand = (++hand) % pagetable.size();
+                }
+                else if (curr_pte->REFERENCED != 0 )
+                {
+                    curr_pte->REFERENCED = 0;
+                    hand = (++hand) % pagetable.size();
+                }
+                else
+                {
+                    cout << "found pte:" << curr_pte->idx << endl;
+                    cout << "PRESENT:" << curr_pte->PRESENT << endl;
+                    cout << "REFERENCED:" << curr_pte->REFERENCED << endl;
+                    cout << "pointing to frame:" << curr_pte->frame_index << endl;
+                    frame_to_replace = &frametable[curr_pte->frame_index];
+                    cout << "frame_to_replace's pte_ptr:" << frame_to_replace->pte_ptr->idx << endl;
+                    break;
+                }
+
+            }
+            // found frame, point to next
+            hand = (hand + 1) % pagetable.size();
+
+            // cout << "frame_to_replace->pte_ptr: " << frame_to_replace->pte_ptr -> idx<< endl;
+            frame_to_replace->pte_ptr->PRESENT = 0;
+            UNMAP(frame_to_replace, stat, output);
+            if (frame_to_replace->pte_ptr->MODIFIED != 0)
+            {
+                OUT(frame_to_replace, stat, output);
+            }
+            return frame_to_replace;
+        }
+};
+
+
 void test_free_list()
 {
     int i = 0;
@@ -142,7 +353,7 @@ void test_free_list()
     cout << endl;
 }
 
-void print_pagetable(bool end_result=false, char alg=' ', stats stat={0,0,0,0,0})
+void print_pagetable()
 {
     for (int i=0; i<pagetable.size(); i++)
     {
@@ -188,6 +399,10 @@ void print_pagetable(bool end_result=false, char alg=' ', stats stat={0,0,0,0,0}
         }
     }
     cout << endl;
+}
+
+void print_frametable(string alg, bool debug)
+{
     for (int i=0; i < frametable.size(); i++)
     {
         if (frametable[i].pte_ptr == 0)
@@ -199,48 +414,80 @@ void print_pagetable(bool end_result=false, char alg=' ', stats stat={0,0,0,0,0}
             cout << frametable[i].pte_ptr->idx << ' ';
         }
     }
-    if (alg=='f' && !end_result)
+    if ((alg=="f"|| alg == "s") && debug)
     {
         cout << " || ";
         for (int i=0; i<fifo_queue.size(); i++)
         {
             cout << fifo_queue[i] << ' ';
         }
-        cout << endl;
     }
-    if (end_result)
+
+    if ((alg=="c" || alg == "X") && debug)
     {
-        cout << endl;
-        long long totalcost = 400*(stat.unmaps+stat.maps) + 3000*(stat.ins+stat.outs) + 150*stat.zeros+inst_count;
-        printf("SUM %d U=%d M=%d I=%d O=%d Z=%d ===> %llu\n",
-        inst_count, stat.unmaps, stat.maps, stat.ins, stat.outs, stat.zeros, totalcost);
+        cout << " || hand = " << hand;
     }
+    cout << endl;
+}
+
+void print_sum(stats stat)
+{
+    // cout << endl;
+    long long totalcost = 400*(stat.unmaps+stat.maps) + 3000*(stat.ins+stat.outs) + 150*stat.zeros+inst_count;
+    printf("SUM %d U=%d M=%d I=%d O=%d Z=%d ===> %llu\n",
+    inst_count, stat.unmaps, stat.maps, stat.ins, stat.outs, stat.zeros, totalcost);
 }
 
 
 frame* allocate_from_free_list()
 {
-    // frame* frame_to_return = &freelist[0];
     if (freelist.empty())
     {
         return nullptr;
     }
     frame* frame_to_return = freelist.front();
     freelist.erase (freelist.begin());
-    // cout << "frame from free list:" << frame_to_return->page_index << endl;
     return frame_to_return;
 }
 
 
-frame* get_frame(frame* old_frame, stats* stat)
+frame* get_frame(frame* old_frame, stats* stat, string alg, int framesize, bool output)
 {
     frame* _frame = allocate_from_free_list();
 
     if (_frame == nullptr)
     {
         // no free frames, got to get old frames
-        FIFO fifo = FIFO();
-        _frame = fifo.allocate_frame(old_frame, stat);
+        if (alg == "f")
+        {
+            FIFO fifo = FIFO();
+            _frame = fifo.allocate_frame(old_frame, stat, output);
+        }
+        else if (alg == "r")
+        {
+            RANDOM random_alg = RANDOM();
+            _frame = random_alg.allocate_frame(old_frame, stat, framesize, output);
+        }
+        else if (alg == "s")
+        {
+            SC sc_alg = SC();
+            _frame = sc_alg.allocate_frame(old_frame, stat, output);
+        }
+        else if (alg == "N")
+        {
+            NRU nru_alg = NRU();
+            _frame = nru_alg.allocate_frame(old_frame, stat, framesize, output);
+        }
+        else if (alg == "c")
+        {
+            CLOCK_PHYS clk_phys_alg = CLOCK_PHYS();
+            _frame = clk_phys_alg.allocate_frame(old_frame, stat, output);
+        }
+        else if (alg == "X")
+        {
+            CLOCK_VIRTUAL clk_vir_alg = CLOCK_VIRTUAL();
+            _frame = clk_vir_alg.allocate_frame(old_frame, stat, output);
+        }
     }
     else
     {
@@ -261,15 +508,56 @@ int main(int argc, char *argv[])
 	opterr = 0;
     string param;
 
-    while ((c = getopt (argc, argv, "aof")) != -1)
+    int framesize=4;
+    bool ordinary_flag=false;
+    bool print_flag=false;
+    bool debug_print_flag=false;
+    bool sum_flag=false;
+    bool frametable_flag=false;
+    bool debug_frametable_flag=false;
+    string alg;
+    string tmp;
+
+    while ((c = getopt (argc, argv, "a:o:f:")) != -1)
     {
         switch (c)
         {
-            case 'o':
-                // cout << "verbose" << endl;
+            case 'a':
+                alg = optarg;
                 break;
-            case 'O':
-                param = optarg;
+            case 'o':
+                // {
+                // string str(optarg);
+                tmp = optarg;
+                if (tmp.find("O", 0, 1)!= string::npos)
+                {
+                    ordinary_flag=true;
+                }
+                if (tmp.find("p", 0, 1)!= string::npos)
+                {
+                    debug_print_flag=true;
+                }
+                if (tmp.find("P", 0, 1)!=string::npos)
+                {
+                    print_flag=true;
+                }
+                if (tmp.find("S", 0, 1)!=string::npos)
+                {
+                    sum_flag=true;
+                }
+                if (tmp.find("f", 0, 1)!=string::npos)
+                {
+                    debug_frametable_flag=true;
+                }
+                if (tmp.find("F", 0, 1)!=string::npos)
+                {
+                    frametable_flag=true;
+                }
+
+                break;
+                //}
+            case 'f':
+                framesize = atoi(optarg);
                 break;
             default:
                 abort ();
@@ -298,7 +586,7 @@ int main(int argc, char *argv[])
     randFile.close();
 
     // initiate frametable, freelist, pagetable
-    for (int i=0; i<16; i++)
+    for (int i=0; i<framesize; i++)
     {
         frame a_frame = frame(i);
         // a_frame.page_index = i;
@@ -316,6 +604,16 @@ int main(int argc, char *argv[])
         pte initial_pte = {0, 0, 0, 0, i};
         pagetable.push_back(initial_pte);
     }
+
+    // initiate NRU class
+    /*
+    if (alg=="N")
+    {
+        for (int i=0; i<64; i++)
+        {
+            class_0.push_back(pagetable[i]);
+        }
+    }*/
 
     stats stat = {0,0,0,0,0};
 
@@ -344,8 +642,12 @@ int main(int argc, char *argv[])
             }
             if (break_flag == false){
                 // test_free_list();
-                cout << "==> inst: " << tokens[0] << " " << tokens[1] << endl;
+                if (ordinary_flag)
+                {
+                    cout << "==> inst: " << tokens[0] << " " << tokens[1] << endl;
+                }
                 pte *curr_pte = &pagetable[stoi(tokens[1])];
+                curr_pte->REFERENCED=1;
                 if (stoi(tokens[0]) == 1) //write operation
                 {
                     curr_pte->MODIFIED=1;
@@ -353,20 +655,29 @@ int main(int argc, char *argv[])
                 if (curr_pte->PRESENT == 0) //pte not present
                 {
                     frame *old_frame, *new_frame;
-                    new_frame = get_frame(old_frame, &stat);
+                    new_frame = get_frame(old_frame, &stat, alg, framesize, ordinary_flag);
                     if (curr_pte->PAGEDOUT != 0)
                     {
-                        IN(curr_pte, new_frame, &stat);
+                        IN(curr_pte, new_frame, &stat, ordinary_flag);
                     }
                     else
                     {
-                        ZERO(new_frame, &stat);
+                        ZERO(new_frame, &stat, ordinary_flag);
                     }
 
-                    MAP(curr_pte, new_frame, &stat);
+                    MAP(curr_pte, new_frame, &stat, ordinary_flag);
 
                 }
-                print_pagetable(false, 'f', stat);
+                if (debug_print_flag)
+                {
+                    print_pagetable();
+                }
+                // TODO
+                //if (frametable_flag && debug_print_flag)
+                if (debug_frametable_flag)
+                {
+                    print_frametable(alg, debug_frametable_flag);
+                }
                 inst_count++;
             }
             else
@@ -375,6 +686,18 @@ int main(int argc, char *argv[])
             }
         }
     }
-    print_pagetable(true, 'f', stat);
+    if (print_flag)
+    {
+        print_pagetable();
+    }
+    if (frametable_flag)
+    {
+        debug_frametable_flag = false;
+        print_frametable(alg, debug_frametable_flag);
+    }
+    if (sum_flag)
+    {
+        print_sum(stat);
+    }
 	inFile.close();
 }
